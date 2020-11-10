59e47c8e594183bec35d0575d92dc4e2a5c1c35e
Tyler Zoucha <tylerzoucha@gmail.com>
2020-11-09 21:05:06 -0600

    Update

--- CSCI-3320-ZC-F20-PA5/src/Heapsort.java
+++ CSCI-3320-ZC-F20-PA5/src/Heapsort.java
@@ -1,115 +1,114 @@
 import java.util.Scanner;
 
-// Java program for implementation of Heap Sort
-public class Heapsort {
-    public void sort(int arr[]) {
-        int n = arr.length;
+/* Class HeapSortWithRange */
+public class Heapsort
+{
+    private static int SIZE;
 
-        // Build heap (rearrange array)
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
+    /* Sort Function */
+    public static void sortwithrange(int origin_arr[], int low, int high)
+    {
+        int size_arr = origin_arr.length;
+        int range_arr[] = new int[ size_arr ];
 
-        // One by one extract an element from heap
-        for (int i=n-1; i>=0; i--) {
-            // Move current root to end
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+        int inc=0;
+        for (int i = 0; i < size_arr; i++)
+        {
+            if(origin_arr[i]>=low && origin_arr[i]<=high){
+                range_arr[inc] = origin_arr[i];
+                inc++;
+            }
+        }
 
-            // call max heapify on the reduced heap
-            heapify(arr, i, 0);
+        for(int i=0; i<range_arr.length; i++){
+            origin_arr[i] = range_arr[i];
+        }
+
+        heapify(origin_arr);
+        for (int i = SIZE; i > 0; i--)
+        {
+            swaparray(origin_arr,0, i);
+            SIZE = SIZE-1;
+            maxheaparr(origin_arr, 0);
         }
     }
 
-    // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
-    void heapify(int arr[], int n, int i) {
-        int largest = i; // Initialize largest as root
-        int l = 2*i + 1; // left = 2*i + 1
-        int r = 2*i + 2; // right = 2*i + 2
 
-        // If left child is larger than root
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
-
-        // If right child is larger than largest so far
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
-
-        // If largest is not root
-        if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-
-            // Recursively heapify the affected sub-tree
-            heapify(arr, n, largest);
-        }
+    public static void heapify(int origin_arr[])
+    {
+        SIZE = origin_arr.length-1;
+        for (int i = SIZE/2; i >= 0; i--)
+            maxheaparr(origin_arr, i);
     }
 
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
+    public static void maxheaparr(int origin_arr[], int i)
+    {
+        int left = 2*i ;
+        int right = 2*i + 1;
+        int max = i;
+        if (left <= SIZE && origin_arr[left] > origin_arr[i])
+            max = left;
+        if (right <= SIZE && origin_arr[right] > origin_arr[max])
+            max = right;
+
+        if (max != i)
+        {
+            swaparray(origin_arr, i, max);
+            maxheaparr(origin_arr, max);
+        }
+    }
+    /* To swap two numbers in an array */
+    public static void swaparray(int origin_arr[], int i, int j)
+    {
+        int tmp = origin_arr[i];
+        origin_arr[i] = origin_arr[j];
+        origin_arr[j] = tmp;
+    }
+    /* Main method */
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner( System.in );
+        System.out.println("Heap Sort With Range\n");
+        int n, i, low_val, high_val;
+
+        /* Accept number of elements */
+        System.out.println("Enter number of integer elements");
+        n = scan.nextInt();
+
+        /* Make array of n elements */
+        int origin_arr[] = new int[ n ];
+        int temp_arr[] = new int[ n ];
+
+        /* Accept elements */
+        System.out.println("\nEnter "+ n +" integer elements");
+        for (i = 0; i < n; i++)
+            origin_arr[i] = scan.nextInt();
+
+        /*Get range*/
+        System.out.println("Enter ranges, low:");
+        low_val = scan.nextInt();
+        System.out.println("Enter high(it should be greater than low element):");
+        high_val = scan.nextInt();
+        if(high_val<low_val)
+        {
+            do{
+                System.out.println("Invalid range");
+                System.out.println("Enter high(it should be greater than low element):");
+                high_val = scan.nextInt();
+            }while(high_val<low_val);
+        }
+
+        /*Copy original array to temp array*/
+        temp_arr = origin_arr.clone();
+        sortwithrange(temp_arr, low_val, high_val);
+
+        /*sort and display the array with in range*/
+        System.out.println("\nElements after sorting ");
+        for (i = 0; i < n; i++)
+            if(temp_arr[i]>0)
+                System.out.print(temp_arr[i]+" ");
+
+
         System.out.println();
-    }
-
-    // Driver program
-    public static void main(String args[]) {
-        // int arr[] = {12, 11, 13, 5, 6, 7};
-        //int n = arr.length;
-
-        int n;///variable declaration
-        Scanner sc = new Scanner(System.in);
-
-        //reading number
-        System.out.println("Enter size of the array:");
-        n =sc.nextInt();
-
-
-
-        System.out.println("Enter array elements : ");
-
-        int arr[] = new int[n];//array declaration
-        int i;
-        for(i=0;i<n;i++)
-            arr[i] =sc.nextInt();//inserting into array.........
-
-
-        //asking low,high values..
-        int low,high;
-
-        System.out.print("Enter low value : ");
-        low = sc.nextInt();
-        System.out.print("Enter high value : ");
-        high = sc.nextInt();
-
-        if(low<n && high < n && low<=high) {
-
-            int narr[] = new int[high-low+1];//duplicate array for sorting...
-            i=0;
-            int l = low;
-            while(l<=high) {
-                narr[i++]=arr[l++];//copying
-            }
-
-            Heapsort ob = new Heapsort();
-            ob.sort(narr);//performing heapsort....
-
-
-            //now copying sorted range values to original array
-            i=0;
-            l = low;
-            while(l<=high) {
-                arr[l++]=narr[i++];//copying
-            }
-
-            System.out.println("Sorted array from given range:");
-            System.out.println("Low :"+low+" High :"+high+" is :");
-            printArray(arr);
-        }
-        else
-            System.out.println("You have entered invalid range try Again..\n");
     }
 }
