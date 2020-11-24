8043447f8e19014a600d4a27c910604c0b1492ab
Tyler Zoucha <tylerzoucha@gmail.com>
2020-11-23 20:27:07 -0600

    updot

--- CSCI-3320-ZC-F20-PA6/src/Graph.java
+++ CSCI-3320-ZC-F20-PA6/src/Graph.java
@@ -1,48 +1,132 @@
-import java.util.LinkedList;
+import java.util.ArrayList;
+
+/**
+ * This class represents a graph with vertices and edges.
+ */
 
 public class Graph {
-    int vertex;
-    LinkedList<Integer> list[];
+    private ArrayList<Vertex> vertexList;
+    private ArrayList<Edge> edgeList;
 
-    public Graph(int vertex) {
-        this.vertex = vertex;
-        list = new LinkedList[vertex];
-        for (int i = 0; i <vertex ; i++) {
-            list[i] = new LinkedList<>();
+    public Graph() {
+        this.vertexList = new ArrayList<Vertex>();
+        this.edgeList = new ArrayList<Edge>();
+    }
+
+    /**
+     * Adds vertex to the list if it is not already present.
+     *
+     * @param v
+     *            - vertex to be added
+     * @return - Returns true if vertex is added, false otherwise
+     */
+    public boolean addVertex(Vertex v) {
+        // Check if vertex is already present
+        if (!vertexList.contains(v)) {
+            vertexList.add(v);
+            return true;
         }
+        return false;
     }
 
-    public void addEdge(int source, int destination){
 
-        //add edge
-        list[source].addFirst(destination);
-
-        //add back edge ((for undirected)
-        list[destination].addFirst(source);
+    /**
+     * Adds an edge to the list if it is not already present.
+     *
+     * @param e
+     *            - edge to be added
+     * @return - Returns true if edge is added, false otherwise
+     */
+    public boolean addEdge(Edge e) {
+        // Check if edge is already present
+        if (!edgeList.contains(e)) {
+            // Try to add edge to the from vertex
+            if (e.getFrom().addEdge(e)) {
+                edgeList.add(e);
+            } else
+                return false;
+        }
+        return false;
     }
 
-    public void printGraph(){
-        for (int i = 0; i <vertex ; i++) {
-            if(list[i].size()>0) {
-                System.out.print("Vertex v" + i + " is connected to: ");
-                for (int j = 0; j < list[i].size(); j++) {
-                    System.out.print("v");
-                    System.out.print(list[i].get(j) + " ");
-                }
+    /**
+     * Displays the graph
+     */
+    public void displayGraph() {
+        if (vertexList.size() > 0) {
+            for (Vertex v : vertexList) {
+                // Check if vertex v has edges
+                if (v.getEdgeList().size() > 0) {
+                    System.out.println("Vertex " + v
+                            + " has edges to the following vertices:");
+                    // Display edges
+                    for (Edge e : v.getEdgeList()) {
+                        System.out.println("\tVertex " + e.getTo()
+                                + " with weight " + e.getWeight());
+                    }
+                } else
+                    System.out.println("Vertex " + v
+                            + " has no outgoing edges to any of the vertices.");
                 System.out.println();
             }
         }
     }
 
-    public static void main(String[] args) {
-        Graph graph = new Graph(5);
-        graph.addEdge(0,1);
-        graph.addEdge(0, 4);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 3);
-        graph.addEdge(3, 4);
-        graph.printGraph();
+    /*
+     * Implementation of the dijstra method
+     */
+    void dijkstra( Vertex s )
+    {
+        ArrayList<Vertex> unknownVertexList = new ArrayList<Vertex>();
+        for(Vertex v : vertexList)
+        {
+            v.dist = Integer.MAX_VALUE;
+            unknownVertexList.add(v);
+        }
+        s.dist = 0;
+        while( unknownVertexList.size()>0 )
+        {
+            Vertex v = getMinVertex(unknownVertexList);
+            //v.known = true;
+            unknownVertexList.remove(v);
+            // for each Vertex w adjacent to v
+            for(Edge e : v.getEdgeList()){
+                Vertex w;
+                w = (e.getTo().equals(v))?e.getFrom():e.getTo();
+                int cvw = e.getWeight();
+
+                if( v.dist + cvw < w.dist )
+                {
+                    w.dist = v.dist + cvw;
+                    w.path = v;
+                }
+            }
+        }
+    }
+
+    /*
+     * This method returns the vertex with minimum distance in unknownVertexList
+     */
+    Vertex getMinVertex(ArrayList<Vertex> vertexList){
+        int minDist = Integer.MAX_VALUE;
+        Vertex minVertex = null;
+        for(Vertex v: vertexList){
+            if(v.dist < minDist){
+                minDist = v.dist;
+                minVertex = v;
+            }
+        }
+        return minVertex;
+    }
+
+    ArrayList<Vertex> getPath(Vertex w){
+        ArrayList<Vertex> path = new ArrayList<Vertex>();
+        path.add(0,w);
+        Vertex prev=w.path;
+        while(prev!=null){
+            path.add(0,prev);
+            prev = prev.path;
+        }
+        return path;
     }
 }
--- CSCI-3320-ZC-F20-PA6/src/mainDriver.java
+++ CSCI-3320-ZC-F20-PA6/src/mainDriver.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 // Acts as the front end for the user to interact with
 public class mainDriver {
-    private int size;
+    private static int size;
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         String userInput;
         boolean valid = false;
-    do {
-        System.out.println("Please enter nodes sepated by commas...");
-        userInput=input.nextLine();
+        do {
+            System.out.println("Please enter nodes separated by commas; i.e., A,B,C,D,E,F,G);
+            userInput = input.nextLine();
 
-        if(userInput.contains(" "))
-        System.out.println("\nPlease try again without any space.\n");
-        else
-        break;
+            if (userInput.contains(""))
+                System.out.println("\nPlease try again without any space.\n");
+            else
+                break;
         } while (!valid);
 
-    int elements[] = userInput.split(",");
-
-
-
-
-
+        String[] elements = userInput.split(",");
+        size = elements.length;
+        System.out.println("Input edges with weights to determine pathways; i.e., AB 2, AD 1, BE 10, ... GE 1" +);
+        System.out.println("Enter the starting node for the algorithm; i.e., B");
+        System.out.println("The shortest known paths are:");
+    }
 }
